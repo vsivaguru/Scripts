4cf05cce4f842620c41a77bb29388cceb4f4ac40
sivaguru.vinayagam <34693602+vsivaguru@users.noreply.github.com>
2020-01-15 15:25:31 +0100

    Update install_kube.sh

--- install_kube.sh
+++ install_kube.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+#$1: last result
+#$2: awaited result
+#$3: custom message
+checkResult() {
+    if [ $1 -ne $2 ]
+    then
+        msg=$3
+        if [[ -z "$1" ]]; then
+            msg="See output log"
+        fi
+        echo -e "Error occured: $msg."
+        exit 1
+    fi
+}
+
+
 #Remove any older installations of Docker that may be on your system
 echo "[TASK 1] Remove any older installations of Docker that may be on your system"
 read -p "Appuyer sur une touche pour continuer ..."
 sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
+
+checkResult $? 0 "Git error"
 
 #Install packages to allow apt to use a repository over HTTPS
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -20,14 +38,16 @@ echo "[TASK 3] Enable and start docker service"
 read -p "Appuyer sur une touche pour continuer ..."
 systemctl enable docker >/dev/null 2>&1
 systemctl start docker
-sudo systemctl status docker.service
+
+ if [ $? -eq 0 ]
+    then 
+        sudo systemctl unmask docker.service
+        sudo systemctl unmask docker.socket
+        sudo systemctl start docker.service
+    fi
+
 
 sleep 5
-
-if [ test ]
-then
-        echo "C'est vrai"
-fi
 
 #Création et ajout d'un user utilisés par RKE pour administrer les nœud du cluster
 echo "Création et ajout d'un user utilisés par RKE pour administrer les nœud du cluster"
@@ -38,8 +58,8 @@ usermod -aG docker manager
 mkdir -p /home/manager/.ssh
 
 #vérifier le manager est bien parti de group docker
-getent group docker
-sleep 5
+getent group docker | grep manager | wc -l
+checkResult $? 1 "Manager n'appartient pas au groupe root"
 
 # Add apt repo file for Kubernetes
 echo "[TASK 5] Add apt repo file for Kubernetes"
@@ -50,7 +70,7 @@ EOF
 apt update
 
 #Add Xenial Kubernetes Repository on both the nodes
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+#sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 # Install Kubernetes
 echo "[TASK 6] Install Kubernetes (kubeadm, kubelet and kubectl)"
@@ -66,7 +86,7 @@ sleep 5
 
 # Install additional required packages
 echo "[TASK 8] Install additional packages"
-yum install -y -q which net-tools sudo sshpass less >/dev/null 2>&1
+apt install -y -q which net-tools sudo sshpass less >/dev/null 2>&1
 
 
 #Unable to start Docker Service in Ubuntu 16.04
